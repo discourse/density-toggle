4764059f75834eb6dd8d66ae6b58ea0583b8a743
Jarek Radosz <jarek@cvx.dev>
2025-10-08 19:51:58 +0200

    DEV: Update linting

--- javascripts/discourse/api-initializers/topic-list-spacing.js
+++ javascripts/discourse/api-initializers/topic-list-spacing.js
@@ -3,7 +3,7 @@ import ClassAdder from "../components/class-adder";
 import DensityMenu from "../components/density-menu";
 import TopicListSpacing from "../components/topic-list-spacing";
 
-export default apiInitializer("1.8.0", (api) => {
+export default apiInitializer((api) => {
   api.renderInOutlet("user-preferences-interface-top", TopicListSpacing);
   api.renderInOutlet("above-main-container", ClassAdder);
   api.renderInOutlet("sidebar-footer-actions", DensityMenu);
